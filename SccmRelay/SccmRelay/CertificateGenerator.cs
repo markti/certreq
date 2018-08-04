3eb47fae94643c1ce3da89b962fb0cdd745aec9b
Mark Tinderholt <markti@outlook.com>
2018-08-04 17:41:18 -0400

    updated to generate a newly generated folder to execute the powershell from

--- SccmRelay/SccmRelay/CertificateGenerator.cs
+++ SccmRelay/SccmRelay/CertificateGenerator.cs
@@ -19,18 +19,58 @@ namespace SccmRelay
             var templateName = ConfigurationManager.AppSettings["TemplateName"];
             var caServerName = ConfigurationManager.AppSettings["CertificateAuthority"];
 
+            // C:\CERTREQ
+            var rootPath = ConfigurationManager.AppSettings["CertificatePath"];
+            var rootDirectory = new DirectoryInfo(rootPath);
+            // C:\CERTREQ\Request.Certificate.ps1
+            var powershellFilePath = rootDirectory.FullName + "\\Request-Certificate.ps1";
+            var powershellFile = new FileInfo(powershellFilePath);
+
             try
             {
+                // C:\CERTREQ\{GUID}
+                var newDirectory = rootDirectory.CreateSubdirectory(Guid.NewGuid().ToString());
+                
+                Console.WriteLine("Creating new sub-directory: " + newDirectory.FullName);
+
+                var newPowerShellExe = newDirectory.FullName + "\\powershell.exe";
+                var newPowerShellFilename = newDirectory.FullName + "\\Request-Certificate.ps1";
+
+                // C:\CERTREQ\{GUID}\Request.Certificate.ps1
+                var newPowershellFile = powershellFile.CopyTo(newPowerShellFilename);
+
+                Console.WriteLine("Copied PowerShell File: " + newPowershellFile.FullName);
+                
+                StringBuilder commandBuilder = new StringBuilder();
+                commandBuilder.Append("-ExecutionPolicy bypass");
+                commandBuilder.Append(" ");
+                commandBuilder.Append("-File ");
+                commandBuilder.Append("\"");
+                commandBuilder.Append(newPowershellFile.FullName);
+                commandBuilder.Append("\"");
+                commandBuilder.Append(" ");
+                commandBuilder.Append("-CN ");
+                commandBuilder.Append(hostname);
+                commandBuilder.Append(" ");
+                commandBuilder.Append("-TemplateName ");
+                commandBuilder.Append(templateName);
+                commandBuilder.Append(" ");
+                commandBuilder.Append("-CAName ");
+                commandBuilder.Append(caServerName);
+                commandBuilder.Append(" ");
+                commandBuilder.Append("-Export");
+
+                var powershellPath = "";
 
                 ProcessStartInfo startInfo = new ProcessStartInfo();
-                startInfo.FileName = @"powershell.exe";
-                startInfo.Arguments = @"-ExecutionPolicy bypass -File ""C:\CERTREQ\Request-Certificate.ps1"" -CN " + hostname + " -TemplateName " + templateName + " -CAName " + caServerName + " -Export";
+                startInfo.FileName = newPowerShellExe;
+                startInfo.Arguments = commandBuilder.ToString();
                 startInfo.RedirectStandardOutput = true;
                 startInfo.RedirectStandardError = true;
                 startInfo.UseShellExecute = false;
                 startInfo.CreateNoWindow = true;
 
-                Console.WriteLine("About to execute Process: " + startInfo.Arguments);
+                Console.WriteLine("PowerShell Arguments:" + startInfo.Arguments);
                 Process process = new Process();
                 process.StartInfo = startInfo;
                 process.Start();
@@ -43,11 +83,14 @@ namespace SccmRelay
 
                 Console.WriteLine(errors);
 
+                // C:\CERTREQ\{GUID}\{hostname}.pfx
+                var certificatePath = newDirectory.FullName + "\\" + hostname + ".pfx";
+                //var certificatePath = "./" + hostname + ".pfx";
 
-                var certFilename = "./" + hostname + ".pfx";
-
-                Console.WriteLine("About to read certificate here: " + certFilename);
-                allBytes = File.ReadAllBytes(certFilename);
+                Console.WriteLine("Converting Certificate File to Bytes:" + certificatePath);
+                
+                Console.WriteLine("About to read certificate here: " + certificatePath);
+                allBytes = File.ReadAllBytes(certificatePath); 
 
             }
             catch (Exception ex)
